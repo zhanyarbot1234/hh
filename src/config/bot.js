dbbd73c266734e355552cfcea8bf49158fb04df4
Mr.HaM4 ✨ <80175341+SoLo077@users.noreply.github.com>
2021-10-12 21:19:14 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: 'NzkxODU2NzU3ODQ0NDc1OTE0.X-VQEw.Rx5pqr6NpLwGv7C-2YA2_-ULLuQ',
-    prefix: 'y'
+    token: 'ODkzNzM5OTU2NzQ3NTAxNTk5.YVf2Mg.g4LD75mjtRRYAokurRem96_xlNI',
+    prefix: '+'
 }
