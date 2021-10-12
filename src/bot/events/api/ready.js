aaead69c9fa43343266c8512e7b99ce0416f8813
Mr.HaM4 ✨ <80175341+SoLo077@users.noreply.github.com>
2021-10-12 21:23:29 +0300

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -5,6 +5,6 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
 
-    await client.user.setActivity("yhelp");
-    await client.user.setStatus("online");
+    await client.user.setActivity("+help");
+    await client.user.setStatus("idle");
 }
