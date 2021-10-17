37796b346a4352a7e9f21b603e9386eba00f3ebc
namo7 <87876783+namo7@users.noreply.github.com>
2021-10-17 10:31:43 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: 'ODk3ODY1ODYwMDM3ODkwMDg4.YWb4vg.WOGD9_Sv4t45eTgeeXRstggHBmE',
+    token: 'ODk3ODY1ODYwMDM3ODkwMDg4.YWb4vg.XNgDPjTQoUZCcO-cY49oY_61fg8',
     prefix: '%'
 }
