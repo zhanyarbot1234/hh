37959d9bf62fb6d05125633a870bec1f5354583c
namo7 <87876783+namo7@users.noreply.github.com>
2021-10-21 18:07:17 +0300

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -5,6 +5,6 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
 
-    await client.user.setActivity("%help | Beta Version");
+   client.user.setActivity(`Type: ${PREFIX}help - %play| ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
     await client.user.setStatus("dnd"); 
 }
