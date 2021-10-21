1015cd83ff4a5ca25421d2ec3b10f6b150a66dd3
namo7 <87876783+namo7@users.noreply.github.com>
2021-10-21 18:18:27 +0300

    Update ready.js

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -5,6 +5,8 @@ module.exports = async function(client) {
     console.log(chalk.yellow.bold(figlet.textSync("jano")));
     await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Is Ready"));
 
-   client.user.setActivity(`Type: ${PREFIX}help - %play| ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
+        
+    client.user.setActivity(` ${PREFIX}help -PoP Music Bot Is here for fun`, { type: "WATCHING"});
+    client.user.setActivity(` ${PREFIX}play| Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "WATCHING"});
     await client.user.setStatus("dnd"); 
 }
